4b69b43d976918158e60133b106884b3e4edc694
gazm <gorgazm@gmail.com>
2017-12-18 19:06:07 -0500

    update algos

--- StartGAZM.bat
+++ StartGAZM.bat
@@ -6,7 +6,7 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100 
 
-set "command=& .\multipoolminer.ps1 -region us -wallet 1MiNERbHbLbchB8g7WpBw5p9X2AZPHEUHo -username gorgazm -workername MAIN -ssl -type nvidia -algorithm equihash,sib,neoscrypt,decred,lbry,bitcore,lyra2v2,m7m,skein,blake2s,blakecoin,xevan,hmq1725,x17,sia,c11,nist5,groestl,jha,tribus,skunk,keccak,phi,hsr,polytimos,lyra2z,ethash,groestl,pascal,siaclaymore -poolname miningpoolhub,miningpoolhubcoins,zpool,ahashpool,hashrefinery -currency btc,usd -interval 240 -watchdog -donate 20"
+set "command=& .\multipoolminer.ps1 -region us -wallet 1MiNERbHbLbchB8g7WpBw5p9X2AZPHEUHo -username gorgazm -workername MAIN -ssl -type nvidia -algorithm equihash,sib,neoscrypt,decred,lbry,bitcore,lyra2v2,m7m,skein,blake2s,blakecoin,xevan,hmq1725,x17,sia,c11,nist5,groestl,jha,tribus,skunk,phi,hsr,polytimos,lyra2z,pascal,siaclaymore -poolname miningpoolhub,miningpoolhubcoins,zpool,ahashpool,hashrefinery -currency btc,usd -interval 240 -watchdog -donate 20"
 
 pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "msiexec -i https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc/PowerShell-6.0.0-rc-win-x64.msi -qn; %command%"
