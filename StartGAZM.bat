eeb51de7f9e73e083d518de5d18c84355281ee89
gazm <gorgazm@gmail.com>
2018-02-27 16:43:15 -0500

    update bat

--- StartGAZM.bat
+++ StartGAZM.bat
@@ -8,9 +8,12 @@ setx GPU_SINGLE_ALLOC_PERCENT 100
 
 set "command=& .\multipoolminer.ps1 -wallet 1MiNERbHbLbchB8g7WpBw5p9X2AZPHEUHo -username gorgazm -type nvidia -WorkerName MAIN -watchdog"
 
+start pwsh -noexit -executionpolicy bypass -command "& .\reader.ps1 -log 'MultiPoolMiner_\d\d\d\d-\d\d-\d\d\.txt' -sort '^[^_]*_' -quickstart"
+
 pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
-msiexec -i https://github.com/PowerShell/PowerShell/releases/download/v6.0.0/PowerShell-6.0.0-win-x64.msi -qb!
+msiexec -i https://github.com/PowerShell/PowerShell/releases/download/v6.0.1/PowerShell-6.0.1-win-x64.msi -qb!
 pwsh -noexit -executionpolicy bypass -windowstyle maximized -command "%command%"
 
 pause
+
